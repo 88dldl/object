99ba111fe7e8b57b90a94d1cb2c28e4600e87b40
88dldl <chltnrgml0999@gmail.com>
2024-01-05 11:28:38 +0900

    1장. 객체,설계

--- Chapter1/01/Invitation.java
+++
@@ -1,7 +0,0 @@
-package org.example.service;
-
-import java.time.LocalDate;
-
-public class Invitation {
-    private LocalDate when; //초대일자
-}
--- Chapter1/03/Invitation.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-import java.time.LocalDate;
-
-public class Invitation {
-    private LocalDate when; //초대일자
-}
